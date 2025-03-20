fa61afa873fb4850d50e8c3d55d343d6e7305ee7
azamatdavronov1 <147363535+azamatdavronov1@users.noreply.github.com>
2025-03-20 21:41:48 +0500

    Update fourth.py

--- lesson-17/homework/fourth.py
+++ lesson-17/homework/fourth.py
@@ -1,1 +1,17 @@
+# import pandas as pd
+#
+# # Titanic Pipeline
+# titanic = pd.read_csv("titanic.csv")
+# result_titanic = (
+#     titanic[titanic["Survived"] == 1]
+#     .assign(Age=lambda x: x["Age"].fillna(x["Age"].mean()))
+#     .assign(Fare_Per_Age=lambda x: x["Fare"] / x["Age"])
+# )
+#
+# # Flights Pipeline
+# flights = pd.read_csv("flights.csv")
+# result_flights = (
+#     flights[flights["DepDelay"] > 30]
+#     .assign(Delay_Per_Hour=lambda x: x["DepDelay"] / x["ActualElapsedTime"])
+# )
 
