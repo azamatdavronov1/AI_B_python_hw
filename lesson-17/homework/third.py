236588874dc69a6996ab32198e8400cbce40551c
azamatdavronov1 <147363535+azamatdavronov1@users.noreply.github.com>
2025-03-20 21:41:32 +0500

    Update third.py

--- lesson-17/homework/third.py
+++ lesson-17/homework/third.py
@@ -1,1 +1,25 @@
 
+# import pandas as pd
+#
+# # Titanic Grouping
+# titanic = pd.read_csv("titanic.csv")
+# grouped_titanic = titanic.groupby("Pclass").agg(
+#     AverageAge=("Age", "mean"),
+#     TotalFare=("Fare", "sum"),
+#     PassengerCount=("PassengerId", "count")
+# ).reset_index()
+#
+# # Movie Multi-level Grouping
+# movies = pd.read_csv("movie.csv")
+# grouped_movies = movies.groupby(["color", "director_name"]).agg(
+#     TotalReviews=("num_critic_for_reviews", "sum"),
+#     AverageDuration=("duration", "mean")
+# ).reset_index()
+#
+# # Flights Nested Grouping
+# flights = pd.read_csv("flights.csv")
+# grouped_flights = flights.groupby(["Year", "Month"]).agg(
+#     TotalFlights=("Flight", "count"),
+#     AverageArrivalDelay=("ArrDelay", "mean"),
+#     MaxDepartureDelay=("DepDelay", "max")
+# ).reset_index()
